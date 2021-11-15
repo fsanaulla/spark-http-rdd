c5b47d415afdd2b0472cd3c1933e7c65ff2608d8
Scala Steward <me@scala-steward.org>
2021-11-15 02:21:43 +0100

    Update testcontainers-scala-scalatest to 0.39.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val scalaTest           = "3.2.10"
     val mockServerNetty     = "5.11.1"
-    val testContainersScala = "0.39.8"
+    val testContainersScala = "0.39.12"
   }
 
   val scalaTest = "org.scalatest" %% "scalatest" % Versions.scalaTest
