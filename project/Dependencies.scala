5dbc5e1ea045355229348cced55eec8364dcd229
Scala Steward <me@scala-steward.org>
2022-01-24 13:02:18 +0100

    Update scalatest to 3.2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val scalaTest           = "3.2.10"
+    val scalaTest           = "3.2.11"
     val mockServerNetty     = "5.11.1"
     val testContainersScala = "0.39.12"
   }
